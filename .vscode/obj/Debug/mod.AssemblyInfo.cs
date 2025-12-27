e665712ce03c0eaedd2c38d14b8f94044b311ebf
Keno <neocv9@gmail.com>
2025-12-27 02:44:06 -0300

    feat: Update research prerequisites for staircase upgrades and associated buildings

--- .vscode/obj/Debug/mod.AssemblyInfo.cs
+++ .vscode/obj/Debug/mod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecondFloor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0.0+ed911b3dce796c6de8f8996504f99f7bfaf7cdde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0.0+88ec7a89f8e8fb5c5bdf11645fdc5b04b3b91448")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecondFloor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecondFloor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.1.0.0")]
