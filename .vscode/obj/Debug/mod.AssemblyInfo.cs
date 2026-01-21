d2c6a74da8297b11cdaa5fdfed6d9e3d53b7b161
Keno <neocv9@gmail.com>
2026-01-21 16:36:05 -0300

    feat: Update gravship staircase icons

--- .vscode/obj/Debug/mod.AssemblyInfo.cs
+++ .vscode/obj/Debug/mod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecondFloor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0.0+58225f178e0dd5b3e3272bde800eff31e524fda7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0.0+a730a666c5b065ae59ebeef12cac75be123c7553")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecondFloor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecondFloor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.1.0.0")]
