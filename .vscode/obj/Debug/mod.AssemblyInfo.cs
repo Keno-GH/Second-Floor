c56d4b00dcb48511f6abd6630703089a77d75c58
Keno <neocv9@gmail.com>
2026-03-24 11:50:47 -0300

    chore: Update .gitignore to include _PublisherPlus.xml and regenerate assembly info files

--- .vscode/obj/Debug/mod.AssemblyInfo.cs
+++ .vscode/obj/Debug/mod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecondFloor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0.0+1e849f511b20a34efb3527be3f3a8a30b3310963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0.0+2e1efe8a79aabf684c3dd8b488a5782fd6a0ed62")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecondFloor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecondFloor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.1.0.0")]
